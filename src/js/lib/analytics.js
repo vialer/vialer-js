bedb0595cb4faf0108e0cfbbd108a8201292dc7f
Jeroen van Veen <j.veenvan@gmail.com>
2017-07-03 15:30:59 +0200

    Re-added semicolon and comment about the ASI.

--- src/js/lib/analytics.js
+++ src/js/lib/analytics.js
@@ -10,13 +10,13 @@ class Analytics {
      * @param {String} analyticsId - Google Analytics id.
      */
     constructor(app, analyticsId) {
-        // Keep the semi-colon here, because of the self-executing function
-        // on the next line.
         this.app = app
-        (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function() {
+
+        // Keep the semi-colon here to prevent ASI ambiguity.
+        ;(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function() {
                 (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
             m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a, m)
-        })(window, document, 'script', 'https://www.google-analytics.com/analytics.js','ga')
+        })(window, document, 'script', 'https://www.google-analytics.com/analytics.js','ga');
         ga('create', analyticsId, 'auto')
         ga('set', 'checkProtocolTask', function(){})
     }
