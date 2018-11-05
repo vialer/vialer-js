13031e3612bb7c16bc6918dcc7d996f26aabcc54
Jos van Bakel <josvanbakel@protonmail.com>
2018-11-05 15:31:25 +0100

    fix: watch of plugins, yarn on NixOS.

--- default.nix
+++ default.nix
@@ -2,10 +2,20 @@
 
 with nixpkgs;
 
+let
+
+  node10 = nodejs-10_x;
+  yarn10 = yarn.override {
+    nodejs = node10;
+  };
+
+in
+
 mkShell {
   buildInputs = [
     # node runtime.
-    nodejs-10_x
+    node10
+    yarn10
 
     # for compiling optipng, gifsicle and jpegtran
     # autoconf zlib nasm automake
