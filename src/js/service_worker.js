853bee8778a3fa7130914d4404014480bb0941ac
Jos van Bakel <josvanbakel@protonmail.com>
2018-12-07 14:54:41 +0100

    fix: pwa build

--- src/js/service_worker.js
+++ src/js/service_worker.js
@@ -36,7 +36,7 @@ self.addEventListener('activate', function(e) {
 self.addEventListener('fetch', function(event) {
     event.respondWith(
         caches.match(event.request).then(response => {
-            return response || fetch(event.request, {mode: 'no-cors'})
+            return response || fetch(event.request)
         })
     )
 })
